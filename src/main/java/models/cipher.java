259799cc3b68eb1921801af51a4ea40c385833d4
Gideon Okuro <gideonollonde@gmail.com>
2020-06-28 17:14:44 +0300

    pass first test

--- src/main/java/models/cipher.java
+++ src/main/java/models/cipher.java
@@ -1,4 +1,9 @@
 package models;
 
 public class cipher {
+    public cipher(String inputString, int cipherKey) {
+    }
+
+    public String getInputString() {
+    }
 }
--- src/test/java/models/cipherTest.java
+++ src/test/java/models/cipherTest.java
@@ -1,7 +1,19 @@
 package models;
 
-import junit.framework.TestCase;
+import org.junit.*;
+import static org.junit.Assert.*;
 
-public class cipherTest extends TestCase {
+public class cipherTest {
 
+    @Test
+    public void newCipher_instantiatesCorrectly() {
+        cipher testCipher = new cipher("A", 0);
+        assertEquals(true, testCipher instanceof cipher);
+    }
+
+    @Test
+    public void newCipher_getsInputString(){
+        cipher testCipher = new cipher("A", 0);
+        assertEquals("A", testCipher.getInputString());
+    }
 }
